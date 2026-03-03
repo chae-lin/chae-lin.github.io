d28a68fc5cb952023b7c018796aa496a1c548ab2
clkim <gjf0055@gmail.com>
2026-03-03 23:50:05 +0900

    fix. npm build로 수정

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,4 +0,0 @@
-
-import { viteNodeFetch } from "file:///Users/kimchaelin/Documents/study/chae-lin.github.io/node_modules/@nuxt/vite-builder/dist/vite-node.mjs"
-export default () => viteNodeFetch.getManifest()
-    
